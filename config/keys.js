5f17d12e5c3c40077958846c723924682827d240
Mahboubimehdi63 <65135515+Mahboubimehdi63@users.noreply.github.com>
2021-06-20 21:27:10 +0100

    Mern APP

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+	    /**mongoURI : 'mongodb+srv://mahboubi:mehdi199M@mehdimahboubitest.xsmdm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'**/
+
+    mongoURI : 'mongodb+srv://ayyoub:timuu.231052@hajjajicluster.xsmdm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
+
